5275b257b7182e94f923a3974888d5172031fa12
Alok-87 <Alok08ax@gmail.com>
2026-03-31 13:43:50 +0530

    commit for deployment

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated during build
 export const APP_VERSION = "0.1.0";
-export const APP_COMMIT_MESSAGE = "update onboardingform and cashused";
-export const APP_BUILD_HASH = "66e8f8f";
-export const APP_BUILD_TIME = "2026-01-28T17:25:50.919Z";
+export const APP_COMMIT_MESSAGE = "commit  for deployment";
+export const APP_BUILD_HASH = "8b6c9bd";
+export const APP_BUILD_TIME = "2026-03-31T08:13:07.568Z";
